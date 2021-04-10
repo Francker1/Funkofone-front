4c5d984f3b05e1735f024558e3ff2449c3b96d7a
Ítalo Franco <italo.ravina@gmail.com>
2021-04-10 17:06:38 +0200

    feat: add src minified css from bootstrap to project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 
 const App = () => {
   return (
-    <Container>
+    <Container className="p-3">
       <Row>
         <Col>
           <h1>Bienvenido a Funk-o-Fone</h1>
