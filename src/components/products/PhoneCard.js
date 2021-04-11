86a845ced01bdeb5ca84195ccb162c5d4c3b9161
Ítalo Franco <italo.ravina@gmail.com>
2021-04-11 21:27:09 +0200

    feat: add a first version of card with hardcoded data

--- src/components/products/PhoneCard.js
+++ src/components/products/PhoneCard.js
@@ -1,16 +1,39 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Col, Row, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import BtnBubble from '../buttons/bubble/BtnBubble';
 
-const PhoneCard = props => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import './style/phone-card.css';
 
-PhoneCard.propTypes = {
+const PhoneCard = (props) => {
+  return (
+    <Col sm={4} className="mb-5">
+      <Card className="shadow border-0">
+        <Card.Img variant="top" src="https://picsum.photos/id/237/250/150" />
+        <h6 className="card-maker">Apple</h6>
+        <Card.Body>
+          <h4>Phone Name</h4>
+          <p>This is a wider card with supporting text below as a natural lead-in to additional.</p>
+          <p>color: white</p>
+          <h5>50,00 €</h5>
+        </Card.Body>
+        <Row>
+          <Col>
+            <Link to="/phones/edit/15">Editar</Link>
+          </Col>
+          <Col>
+            <BtnBubble url="/phones/15" text="Details" />
+          </Col>
+        </Row>
+        <Card.Footer>
+          <small className="text-muted">ID phone: 0001</small>
+        </Card.Footer>
+      </Card>
+    </Col>
+  );
+};
 
-}
+PhoneCard.propTypes = {};
 
-export default PhoneCard
+export default PhoneCard;
--- src/components/products/PhoneList.js
+++ src/components/products/PhoneList.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Container, Row, Col, CardDeck, Card } from 'react-bootstrap';
+import { Container, Row, Col, CardDeck } from 'react-bootstrap';
 import NavBar from '../navbar/NavBar';
+import PhoneCard from './PhoneCard';
 
-import "./style/phone-list.css";
+import './style/phone-list.css';
 
 const PhoneList = () => {
   return (
@@ -17,45 +18,10 @@ const PhoneList = () => {
         <Row>
           <Col>
             <CardDeck>
-              <Card className="shadow border-0">
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-              </Card>
-              <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This card has supporting text below as a natural lead-in to additional content.{' '}
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-              </Card>
-              <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This card has even longer content than the first to show
-                    that equal height action.
-                  </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                  <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-              </Card>
+              <PhoneCard />
+              <PhoneCard />
+              <PhoneCard />
+              <PhoneCard />
             </CardDeck>
           </Col>
         </Row>
