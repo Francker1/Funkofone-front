4b5e0fb00098165f7764c83316f8dcfb400813d8
Ítalo Franco <italo.ravina@gmail.com>
2021-04-11 14:23:31 +0200

    feat: add cover page with image

--- src/components/cover-page/Cover.js
+++ src/components/cover-page/Cover.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+import './style/cover.css';
+
 const Cover = () => {
   return (
-    <Container className="p-3">
+    <Container className="cover" fluid>
       <Row>
-        <Col>
-          <h1>Bienvenido a Funk-o-Fone</h1>
+        <Col className="cover-msg__container">
+          <Col className="p-4">
+            <h1 className="mb-4">Welcome to Funk-o-Fone</h1>
+            <p>Discover our list of phones with the latest market trends</p>
+            <Link to="/phones/">Browse Phones</Link>
+          </Col>
         </Col>
-        <Link to="/phones/">descubrir</Link>
+        <Col className="p-0">
+          <div className="cover-img" />
+        </Col>
       </Row>
     </Container>
   );
