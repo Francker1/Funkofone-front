d860abefbc23ac0ed5241085e45623c752e7a2b9
Ítalo Franco <italo.ravina@gmail.com>
2021-04-14 17:36:54 +0200

    testing: test in helper getPhones

---
+++ src/tests/helpers/getPhones.test.js
@@ -0,0 +1,10 @@
+import { getPhones } from '../../helpers/getPhones';
+
+describe('Test in fetch Phones helpers', () => {
+  test('should get list of phones', async () => {
+    const phones = await getPhones();
+
+    // In case of api is down, or database dont return data, this test not pass
+    expect(phones.length).toBeGreaterThanOrEqual(1);
+  });
+});
